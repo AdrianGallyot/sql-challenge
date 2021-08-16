b9378af8ba01c6d408e46e314d5e837a15a286e7
Adrian Jude Gallyot <83386111+AdrianGallyot@users.noreply.github.com>
2021-08-16 20:51:19 +0800

    Create Copyscripts.sql

---
+++ EmployeeSQL/Copyscripts.sql
@@ -0,0 +1,29 @@
+/*COPY employees
+FROM 'C:\Users\Public\employees.csv'
+DELIMITER ','
+CSV HEADER;*/
+
+/*COPY Departments
+FROM 'C:\Users\Public\departments.csv'
+DELIMITER ','
+CSV HEADER;*/
+
+/*COPY salaries
+FROM 'C:\Users\Public\salaries.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY titles
+FROM 'C:\Users\Public\titles.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_emp
+FROM 'C:\Users\Public\dept_emp.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY dept_manager
+FROM 'C:\Users\Public\dept_manager.csv'
+DELIMITER ','
+CSV HEADER;*/
